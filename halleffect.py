9e43a9ac26728a4d52e0226f6a4e1980467aeebb
ArnoLand <arnie.landge@gmail.com>
2025-10-28 16:59:31 -0400

    new commit

--- halleffect.py
+++ halleffect.py
@@ -1,22 +1,18 @@
 from machine import Pin
 import time
 
-# --- Pin Setup ---
-HALL_PIN = 27       # Pin connected to OUT of Hall sensor
-LED_PIN = 25        # LED indicator pin (optional)
+hall = Pin(35, Pin.IN)
+led = Pin(25, Pin.OUT)
 
-hall = Pin(HALL_PIN, Pin.IN)   # read sensor output
-led = Pin(LED_PIN, Pin.OUT)    # LED to show detection
-
-# --- Main Loop ---
 while True:
-    sensor_value = hall.value()  # read HIGH or LOW (1 or 0)
+    sensor_value = hall.value()  
     
-    if sensor_value == 0:  # 0 means magnet detected
-        led.value(1)
-        print("Magnet detected!")
-    else:  # 1 means no magnetic field nearby
-        led.value(0)
-        print("No magnet.")
+    if sensor_value == 0:  
+        led.value(1)        
+        print("ts a magnet bro")
+    else:
+        led.value(0)     
+        print("ts not a magnet twin")
     
     time.sleep(0.2)
+
